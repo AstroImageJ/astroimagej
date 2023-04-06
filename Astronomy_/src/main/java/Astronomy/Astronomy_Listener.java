057df75e0cc45fa8d07312ab8740cf5f8537320d
Kevin Eastridge <kevbeastridge@gmail.com>
2023-04-06 17:36:58 -0400

    Cleanup Astronomy_Listener

--- Astronomy_/src/main/java/Astronomy/Astronomy_Listener.java
+++ Astronomy_/src/main/java/Astronomy/Astronomy_Listener.java
@@ -17,75 +17,62 @@ import java.util.concurrent.Executors;
  * @version 1.0
  * @date 2010-Jul-25
  */
-public class Astronomy_Listener implements PlugIn, ImageListener
-    {
+public class Astronomy_Listener implements PlugIn, ImageListener {
     public static final boolean REFRESH = true;
     public static final boolean NEW = false;
     public static final boolean RESIZE = true;
     public static final boolean NORESIZE = false;
+    final static Executor EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 
-    public void run (String arg) {
+    public void run(String arg) {
         if (IJ.versionLessThan("1.35l")) return;
         ImagePlus.addImageListener(this);
-        }
+    }
 
-
-    public void imageOpened(ImagePlus imp)
-        {
-//        IJ.log("Image Opened: "+imp.getTitle());
+    public void imageOpened(ImagePlus imp) {
         boolean autoConvert = true;
         Frame openFrame = imp.getWindow();
-        
-        if (openFrame instanceof ij.gui.PlotWindow || imp.getTitle().startsWith("About") || imp.getTitle().startsWith("Profile of") || imp.getTitle().startsWith("Seeing Profile")) return;
+
+        if (openFrame instanceof ij.gui.PlotWindow || imp.getTitle().startsWith("About") ||
+                imp.getTitle().startsWith("Profile of") || imp.getTitle().startsWith("Seeing Profile")) {
+            return;
+        }
         autoConvert = Prefs.get("Astronomy_Tool.autoConvert", autoConvert);
-        if (autoConvert)
-            {
-//            Class<?> mainComponentClass = openFrame.getClass();
-            if (!(openFrame instanceof astroj.AstroStackWindow))//(mainComponentClass.getName() != "astroj.AstroStackWindow")
-                {
-                    var o = imp.getWindow();
-                    AstroCanvas ac = new AstroCanvas(imp);
-                    imp.setWindow(new AstroStackWindow(imp, ac, NEW, RESIZE));
-                    if (o != null) {
-                        o.close();
-                    }
+        if (autoConvert) {
+            if (!(openFrame instanceof astroj.AstroStackWindow)) {
+                var o = imp.getWindow();
+                AstroCanvas ac = new AstroCanvas(imp);
+                imp.setWindow(new AstroStackWindow(imp, ac, NEW, RESIZE));
+                if (o != null) {
+                    o.close();
                 }
             }
-
         }
-
-    public void imageClosed(ImagePlus imp)
-        {
-        }
-
-    public void imageUpdated(ImagePlus imp)
-        {
-//        IJ.log("Image Updated: "+imp.getTitle());
-        Frame openFrame = imp.getWindow();
-        if (openFrame instanceof ij.gui.PlotWindow || imp.getTitle().startsWith("About ImageJ") || imp.getTitle().startsWith("Profile of") || imp.getTitle().startsWith("Seeing Profile")) return;
-
-//        Class<?> mainComponentClass = openFrame.getClass();
-        if (openFrame instanceof astroj.AstroStackWindow)//mainComponentClass.getName().equals("astroj.AstroStackWindow"))
-            {
-            astroj.AstroStackWindow asw = (astroj.AstroStackWindow) openFrame;
-            if (asw.isReady && !asw.minMaxChanged)
-                {
-//                IJ.log("Actually Updating Image: "+imp.getTitle());
-                ;
-                ex.execute(() -> {
-                    if (IJ.isMacro())
-                        imp.waitTillActivated();
-                    asw.setAstroProcessor(false);
-                });
-                }
-            else
-                {
-                asw.minMaxChanged = false;
-                }
-            }
-
-        }
-        final Executor ex = Executors.newCachedThreadPool();
 
     }
+
+    public void imageClosed(ImagePlus imp) {
+    }
+
+    public void imageUpdated(ImagePlus imp) {
+        Frame openFrame = imp.getWindow();
+        if (openFrame instanceof ij.gui.PlotWindow || imp.getTitle().startsWith("About ImageJ") ||
+                imp.getTitle().startsWith("Profile of") || imp.getTitle().startsWith("Seeing Profile")) {
+            return;
+        }
+
+        if (openFrame instanceof AstroStackWindow asw) {
+            if (asw.isReady && !asw.minMaxChanged) {
+                EXECUTOR_SERVICE.execute(() -> {
+                    if (IJ.isMacro()) imp.waitTillActivated();
+                    asw.setAstroProcessor(false);
+                });
+            } else {
+                asw.minMaxChanged = false;
+            }
+        }
+
+    }
+
+}
 

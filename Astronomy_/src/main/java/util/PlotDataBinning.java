cb32677c8085eb637be90d34625562f07d8d67f7
Kevin Eastridge <kevbeastridge@gmail.com>
2023-06-02 17:12:16 -0400

    Data binning now accumulates values incrementally

--- Astronomy_/src/main/java/util/PlotDataBinning.java
+++ Astronomy_/src/main/java/util/PlotDataBinning.java
@@ -1,6 +1,5 @@
 package util;
 
-import flanagan.analysis.Stat;
 import flanagan.math.ArrayMaths;
 import ij.astro.types.Pair;
 
@@ -61,8 +60,7 @@ public class PlotDataBinning {
             Arrays.setAll(binBounds, i -> (i * finalBinWidth) + xMin);
 
             var bins = new DataBin[nBins];
-            double[] finalX2 = x;
-            Arrays.setAll(bins, i -> new DataBin(finalX2.length, i));
+            Arrays.setAll(bins, DataBin::new);
 
             for (int i = 0; i < x.length; i++) {
                 var p = 0;
@@ -76,15 +74,14 @@ public class PlotDataBinning {
                 if (!accepted) throw new RuntimeException("data did not fit into a bin");
             }
 
-            var binCompleted = Arrays.asList(Arrays.stream(bins).parallel().filter(DataBin::hasData)
-                    // Converts to array first as Stream#toList returns unmodifiable list, and we need to sort it
-                    .map(withErr ? DataBin::binnedDatumErr : DataBin::binnedDatum).toArray(DoubleTriple[]::new));
+            // Order the elements by time and filter out empty bins
+            var binCompleted = Arrays.stream(bins).parallel().filter(DataBin::hasData)
+                    .map(withErr ? DataBin::binnedDatumErr : DataBin::binnedDatum)
+                    .sorted(Comparator.comparingDouble(c -> c.x)).toList();
             var outX = new double[binCompleted.size()];
             var outY = new double[binCompleted.size()];
             var outErr = new double[binCompleted.size()];
 
-            // Order the elements by time
-            binCompleted.sort(Comparator.comparingDouble(c -> c.x));
             for (int i = 0; i < binCompleted.size(); i++) {
                 outX[i] = binCompleted.get(i).x();
                 outY[i] = binCompleted.get(i).y();
@@ -108,43 +105,32 @@ public class PlotDataBinning {
         return o;
     }
 
-    private record DataBin(double[] x, double[] y, double[] err, Holder lastIndex, int binIndex) {
-        public DataBin(int maximumSize, int binIndex) {
-            this(new double[maximumSize], new double[maximumSize], new double[maximumSize], new Holder(), binIndex);
+    private static class DataBin {
+        private final int binIndex;
+        private double totX;
+        private double totY;
+        private double totInvErr2;
+        private double totXErr;
+        private double totYErr;
+        private int count;
+
+        public DataBin(int binIndex) {
+            this.binIndex = binIndex;
         }
 
         public void addData(double xDatum, double yDatum, double errDatum) {
-            x[lastIndex.val] = xDatum;
-            y[lastIndex.val] = yDatum;
-            err[lastIndex.val] = errDatum;
-            lastIndex.val++;
+            totX += xDatum;
+            totY += yDatum;
+            if (errDatum != 0) {
+                totInvErr2 += 1/(errDatum*errDatum);
+                totXErr += xDatum / (errDatum*errDatum);
+                totYErr += yDatum / (errDatum*errDatum);
+            }
+            count++;
         }
 
         public boolean hasData() {
-            return lastIndex.val > 0;
-        }
-
-        public DoubleTriple binnedDatum() {
-            return new DoubleTriple(Stat.mean(Arrays.copyOf(x, lastIndex.val)), Stat.mean(Arrays.copyOf(y, lastIndex.val)), 0);
-        }
-
-        public DoubleTriple binnedDatumErr() {
-            var err = Arrays.copyOf(this.err, lastIndex.val);
-            var totalErr = Arrays.stream(err).sum();
-            var totalErr2 = totalErr * totalErr;
-            var err2 = Arrays.stream(err).map(d -> d*d).toArray();
-            var invErr2 = Arrays.stream(err2).map(d -> d != 0 ? 1/d : 0).toArray();
-            var totInvErr2 = Arrays.stream(invErr2).sum();
-
-            var totX = IntStream.range(0, lastIndex.val).mapToDouble(i -> x[i]/err2[i]).sum();
-            var binX = totX / totInvErr2;
-
-            var totY = IntStream.range(0, lastIndex.val).mapToDouble(i -> y[i]/err2[i]).sum();
-            var binY = totY / totInvErr2;
-
-            var binErr = 1/Math.sqrt(totInvErr2);
-
-            return new DoubleTriple(binX, binY, binErr);
+            return count > 0;
         }
 
         public boolean accept(double[] binBounds, double x, double y) {
@@ -161,9 +147,14 @@ public class PlotDataBinning {
             return false;
         }
 
-        static class Holder {
-            public int val = 0;
+        public DoubleTriple binnedDatum() {
+            return new DoubleTriple(totX/count, totY/count, 0);
         }
+
+        public DoubleTriple binnedDatumErr() {
+            return new DoubleTriple(totXErr / totInvErr2, totYErr / totInvErr2, 1/Math.sqrt(totInvErr2));
+        }
+
     }
 
     private record DoubleTriple(double x, double y, double err) {}

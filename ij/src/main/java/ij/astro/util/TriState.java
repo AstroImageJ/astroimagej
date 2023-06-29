5013eaeb6089909237cabe76ad1d417eef324806
Kevin Eastridge <kevbeastridge@gmail.com>
2023-06-28 23:59:58 -0400

    Change background color of TriState

--- ij/src/main/java/ij/astro/util/TriState.java
+++ ij/src/main/java/ij/astro/util/TriState.java
@@ -7,20 +7,15 @@ import java.awt.*;
 
 //⍻☐☑☒■□▣◽◾🗵🗷🗸◻◼🗹⮽█
 public enum TriState {
-    DISABLED("☐", false),
-    ENABLED("☑", true),
-    ALT_ENABLED("■", "☑", true);
+    DISABLED("■", "☐", false, Color.WHITE),
+    ENABLED("■", "☑", true, Color.WHITE),
+    ALT_ENABLED("■", "☑", true, Color.LIGHT_GRAY);
 
     public final Icon icon;
     private final boolean isOn;
 
-    TriState(String icon, boolean isOn) {
-        this.icon = new EmojiIcon(icon, 19);
-        this.isOn = isOn;
-    }
-
-    TriState(String icon1, String icon2, boolean isOn) {
-        this.icon = new MergedIcon(new EmojiIcon(icon1, 19, Color.LIGHT_GRAY), new EmojiIcon(icon2, 19), -3, 2, true);
+    TriState(String icon1, String icon2, boolean isOn, Color color) {
+        this.icon = new MergedIcon(new EmojiIcon(icon1, 19, color), new EmojiIcon(icon2, 19), -3, 2, true);
         this.isOn = isOn;
     }
 

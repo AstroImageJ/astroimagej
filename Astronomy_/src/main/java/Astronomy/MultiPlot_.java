afa35f1b61bec5a1d979571bb4a6d0558851e050
Kevin Eastridge <kevbeastridge@gmail.com>
2024-01-25 18:46:12 -0500

    Properly constrain inclination for transit modeling

--- Astronomy_/src/main/java/Astronomy/MultiPlot_.java
+++ Astronomy_/src/main/java/Astronomy/MultiPlot_.java
@@ -4506,7 +4506,7 @@ public class MultiPlot_ implements PlugIn, KeyListener {
                     incl = lockToCenter[curve][4] ? priorCenter[curve][4] * Math.PI / 180.0 : param[fp < nPars ? fp++ : nPars - 1];  //inclination
                     b = Math.cos(incl) * ar;
                     if (b > 1.0 + p0) {  //ensure planet transits or grazes the star
-                        incl = Math.acos((1.0 + p0) / ar);
+                        return Double.POSITIVE_INFINITY;
                     }
                 } else {
                     incl = Math.acos(bp[curve]/ar);

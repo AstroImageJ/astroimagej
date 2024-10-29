f34fe9180ad7b1bc613d41d391f24be46de85964
Kevin Eastridge <kevbeastridge@gmail.com>
2024-10-29 17:06:00 -0400

    Update JNA Release mutex on shutdown

--- Astronomy_/src/main/java/util/WinMutexHandler.java
+++ Astronomy_/src/main/java/util/WinMutexHandler.java
@@ -2,6 +2,7 @@ package util;
 
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinNT;
 import ij.IJ;
 
 public class WinMutexHandler {
@@ -10,9 +11,21 @@ public class WinMutexHandler {
      */
     public static void createMutex() {
         try {
-            if (Kernel32.INSTANCE.CreateMutex(null, false, "AstroImageJ") == null) {
+            final WinNT.HANDLE handle;
+
+            if ((handle = Kernel32.INSTANCE.CreateMutex(null, false, "AstroImageJ")) == null) {
                 IJ.error("Failed to create Mutex with error " + Native.getLastError());
             }
+
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                    try {
+                        if (Kernel32.INSTANCE.ReleaseMutex(handle)) {
+                            System.err.println("Failed to release Mutex during shutdown " + Native.getLastError());
+                        }
+                    } catch (Exception | UnsatisfiedLinkError e) {
+                        e.printStackTrace();
+                    }
+            }, "AIJ Win Mutex Freer"));
         } catch (UnsatisfiedLinkError e) {
             e.printStackTrace();
         }

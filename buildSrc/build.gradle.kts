f6862557adda2bcff43381e4b63ad5e301281baa
Kevin Eastridge <kevbeastridge@gmail.com>
2024-12-24 23:29:44 -0500

    Use toolchain for kotlin buildSrc compilation

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     `kotlin-dsl`
 }
 
+kotlin {
+    jvmToolchain(22)
+}
+
 repositories {
     // So that external plugins can be resolved in dependencies section
     gradlePluginPortal()

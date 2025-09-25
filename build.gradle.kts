adc4d6e3d83f863de0f9df23f5a97931649656c6
Kevin Eastridge <kevbeastridge@gmail.com>
2025-09-25 02:54:30 -0400

    Force file permissions in tar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -800,6 +800,17 @@ javaRuntimeSystemsProperty.get().forEach { (_, sysInfo) ->
 
             from(appImageDir) {
                 exclude("*.tgz")
+
+                if (path.contains("astroimagej/bin/") ||
+                    path.endsWith("/jspawnhelper") ||
+                    path.contains("/lib/runtime/lib/")) {
+
+                    filePermissions {
+                        user.execute = true
+                        other.execute = true
+                        group.execute = true
+                    }
+                }
             }
         }
     } else {

f0d7e4871910db366be0f0d4ee75887068f13af3
Kevin Eastridge <kevbeastridge@gmail.com>
2025-09-26 08:39:08 -0400

    Add links to homepage from docs and downloads pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -6,6 +6,12 @@ export default defineConfig({
     integrations: [
         starlight({
             title: 'AstroImageJ Docs',
+            social: [
+                { icon: 'seti:csv', label: 'Home', href: 'https://astroimagej.com/' },
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/AstroImageJ/astroimagej' },
+                { icon: 'comment', label: 'Github Discussions', href: 'https://github.com/AstroImageJ/astroimagej/discussions' },
+                { icon: 'comment-alt', label: 'Nabble Forum', href: 'http://astroimagej.170.s1.nabble.com/' },
+            ],
         }),
     ],
 });

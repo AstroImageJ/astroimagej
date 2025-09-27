a5e9f4ef0871c0b5de8369cde2ea09c80dcfa8bf
Kevin Eastridge <kevbeastridge@gmail.com>
2025-09-27 13:18:11 -0400

    Update home icon

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -7,13 +7,15 @@ export default defineConfig({
         starlight({
             title: 'AstroImageJ Docs',
             social: [
-                { icon: 'seti:csv', label: 'Home', href: 'https://astroimagej.com/' },
                 { icon: 'github', label: 'GitHub', href: 'https://github.com/AstroImageJ/astroimagej' },
                 { icon: 'comment', label: 'Github Discussions', href: 'https://github.com/AstroImageJ/astroimagej/discussions' },
                 { icon: 'comment-alt', label: 'Nabble Forum', href: 'http://astroimagej.170.s1.nabble.com/' },
             ],
             editLink: {
                 baseUrl: 'https://github.com/AstroImageJ/astroimagej/edit/master/website/',
+            },
+            components: {
+                SocialIcons: './src/components/StarlightHeader.astro',
             },
         }),
     ],

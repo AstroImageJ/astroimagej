2168d0e81af720c0d9980a33c5a2835563575a8a
keastrid <70925169+keastrid@users.noreply.github.com>
2025-12-04 16:52:59 -0500

    Improve wait for uninstaller to finish

--- Astronomy_/src/main/resources/Astronomy/updater/windowsMigration.bat
+++ Astronomy_/src/main/resources/Astronomy/updater/windowsMigration.bat
@@ -7,8 +7,15 @@ powershell -NoProfile -Command "Wait-Process -Id %~1"
 REM remove existing version
 if exist "%~3\unins000.exe" (
     start "" /WAIT /B "%~3\unins000.exe"
-    REM wait a bit to give time for the UAC to show correctly
-    TIMEOUT /T 3 /NOBREAK
+    REM loop waiting for uninstaller to exit with timeout
+    set timeout=0
+    :wait_loop
+    if exist "%~3\unins000.exe" (
+        TIMEOUT /T 1 /NOBREAK >nul
+        set /a timeout+=1
+        if !timeout! lss 120 goto wait_loop
+    )
+    TIMEOUT /T 5 /NOBREAK
 ) else (
     REM todo
 )

49af98065d4b5f5f4f0ba4fd6cf15fdf8b2ab0ed
keastrid <kevbeastridge@gmail.com>
2025-12-14 17:10:43 -0500

    Update mac elevator to handle downgrades

--- Astronomy_/src/main/resources/Astronomy/updater/mac.sh
+++ Astronomy_/src/main/resources/Astronomy/updater/mac.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Log this script to file
+#exec &> ~/Desktop/int.log
+
 PID_TO_WAIT="$1"
 DMG="$2"
+DEST="$3"
+DOWNGRADE="$4"
 
-# Wait for the given PID (if any) to exit, silently
-if [[ -n "$PID_TO_WAIT" ]]; then
-  while kill -0 "$PID_TO_WAIT" 2>/dev/null; do
-    sleep 1
+# Downgrade handling: if $DEST exists and this is a downgrade,
+# user must remove app manually otherwise the installer silently fails
+if [[ "$DOWNGRADE" == "true" ]]; then
+  while [[ -d "$DEST" ]]; do
+    if ! osascript >/dev/null <<EOF
+      set theDest to "$(printf '%s' "$DEST" | sed 's/"/\\"/g')"
+      display dialog "
+      You are attempting to downgrade AstroImageJ.
+      Apple does not natively allow app downgrades.
+
+      If you would like to continue:
+      1) Keep this panel open
+      2) In Finder, go to: " & theDest & "
+      3) Move the AstroImageJ app to the Trash
+      4) Then click OK to continue
+
+      Otherwise, click Cancel to abort the downgrade." buttons {"Cancel", "OK"} default button "OK" cancel button "Cancel" with title "AstroImageJ Elevator"
+EOF
+    then
+      exit 0
+    fi
+
+    # If user clicked OK but $DEST still exists, loop and show again.
   done
 fi
 

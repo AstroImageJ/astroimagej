0a015183c0552aff189951d4e03bdfcf861bbae2
keastrid <70925169+keastrid@users.noreply.github.com>
2026-03-08 18:55:53 -0400

    Reformat Read_MeasurementTable

--- Astronomy_/src/main/java/Astronomy/Read_MeasurementTable.java
+++ Astronomy_/src/main/java/Astronomy/Read_MeasurementTable.java
@@ -5,31 +5,28 @@ import ij.IJ;
 import ij.io.OpenDialog;
 import ij.plugin.PlugIn;
 
-public class Read_MeasurementTable implements PlugIn
-	{
-	public void run(String arg)
-		{
+public class Read_MeasurementTable implements PlugIn {
+    public void run(String arg) {
         String path = arg;
-        if (path == null || path.equals(""))
-            {
-            OpenDialog od = new OpenDialog("Select measurement table to be opened",null);
+        if (path == null || path.isEmpty()) {
+            OpenDialog od = new OpenDialog("Select measurement table to be opened", null);
             String dir = od.getDirectory();
             String filename = od.getFileName();
-            if (dir == null || filename == null)
+            if (dir == null || filename == null) {
                 return;
-            path = dir + filename;
             }
-        
-		// IJ.showMessage ("open "+dir+filename);
-		MeasurementTable table = MeasurementTable.getTableFromFile (path);
-		if (table == null)
-			IJ.showMessage ("Unable to open measurement table "+path);
-		else
-            {
-			table.show();
+            path = dir + filename;
+        }
+
+        // IJ.showMessage ("open "+dir+filename);
+        MeasurementTable table = MeasurementTable.getTableFromFile(path);
+        if (table == null) {
+            IJ.showMessage("Unable to open measurement table " + path);
+        } else {
+            table.show();
             if (MultiPlot_.isRunning()) {
                 MultiPlot_.loadDataOpenConfig(table, path);
-                }
             }
-		}
-	}
+        }
+    }
+}

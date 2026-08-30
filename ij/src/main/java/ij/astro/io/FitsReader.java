2926ac1d2fc6c8525b3f93bf49892f0dab57157b
keastrid <70925169+keastrid@users.noreply.github.com>
2026-08-30 17:20:06 -0400

    Remove debug code

--- ij/src/main/java/ij/astro/io/FitsReader.java
+++ ij/src/main/java/ij/astro/io/FitsReader.java
@@ -53,7 +53,6 @@ import ij.io.Opener;
 import ij.measure.ResultsTable;
 import ij.plugin.FITS_Reader;
 import ij.plugin.FolderOpener;
-import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
 import nom.tam.fits.BasicHDU;
 import nom.tam.fits.Fits;
@@ -79,7 +78,6 @@ public class FitsReader implements AutoCloseable {
     private static final PixelPatcher PIXEL_PATCHER = ServiceLoader.load(PixelPatcher.class, IJ.getClassLoader())
             .findFirst()
             .orElseThrow(() -> new RuntimeException("No PixelPatcher implementation found"));
-    private static final boolean DEBUG_BPM = false;
     private final Fits fits;
     private final List<HDUDescriptor> hduDescriptors;
     private final BasicHDU<?>[] hdus;
@@ -206,9 +204,6 @@ public class FitsReader implements AutoCloseable {
 
         var header = descriptor.getFormedHeader();
         if (isBasic3DImage()) {
-            if (DEBUG_BPM && isLco() && findBadPixelMask() != -1) {
-                return 3;
-            }
             return countStackableImagesFromDescriptors();
         }
 
@@ -319,36 +314,18 @@ public class FitsReader implements AutoCloseable {
         if (naxis == 2) {
             var headerText = headerToString(header);
             var headers = List.of(headerText);
-            var bpmMaskIdx = findBadPixelMask();
 
             if (filter != null && !filter.matchesFilter(header)) {
                 return new ProcessedFits(List.of(), List.of());
             }
 
             if (!includeProcessors) {
-                if (DEBUG_BPM && bpmMaskIdx != -1) {
-                    return new ProcessedFits(List.of(), List.of(headerText, headerText, headerText));
-                }
                 return new ProcessedFits(List.of(), headers);
             }
 
             var processor = twoDimensionalImageData2Processor(firstImageIndex);
-            var original = DEBUG_BPM ? new FloatProcessor(processor.getFloatArray()) : null;
-            var mask = processBadPixelMask(processor, bpmMaskIdx);
-
-            if (DEBUG_BPM) {
-                var delta = new FloatProcessor(width, height);
-                for (int i = 0; i < original.getWidth(); i++) {
-                    for (int j = 0; j < original.getHeight(); j++) {
-                        delta.setf(i, j, processor.getf(i, j) - original.getf(i, j));
-                    }
-                }
-
-                if (mask != null) {
-                    return new ProcessedFits(List.of(processor, mask, delta), List.of(headerText, headerText, headerText));
-                }
-            }
-
+            var bpmMaskIdx = findBadPixelMask();
+            processBadPixelMask(processor, bpmMaskIdx);
             return new ProcessedFits(List.of(processor), headers);
         }
 
